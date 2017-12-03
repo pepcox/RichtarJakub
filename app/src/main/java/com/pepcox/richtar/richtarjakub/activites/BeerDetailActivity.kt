27358c683021ceab3f56583324f14b8a1c5e16de
nosko <michal.noskovic@backbone.sk>
2017-12-03 22:52:35 +0100

    Dummy animation while beer detail loading

--- app/src/main/java/com/pepcox/richtar/richtarjakub/activites/BeerDetailActivity.kt
+++ app/src/main/java/com/pepcox/richtar/richtarjakub/activites/BeerDetailActivity.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.text.Html
 import android.view.MenuItem
+import android.view.animation.AnimationUtils
 import android.widget.Toast
 import com.pepcox.richtar.richtarjakub.R
 import com.pepcox.richtar.richtarjakub.RichtarJakupApp
 import com.pepcox.richtar.richtarjakub.data.Beer
 import com.pepcox.richtar.richtarjakub.managers.BeerManager
+import com.squareup.picasso.Callback
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_beer_detail.*
 import rx.android.schedulers.AndroidSchedulers
@@ -21,23 +23,21 @@ class BeerDetailActivity: AppCompatActivity() {
         const val BEER_ARG = "beer"
     }
 
-    private lateinit var beer: Beer
     @Inject
     lateinit var beerManager: BeerManager
+    private lateinit var beer: Beer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_beer_detail)
 
-        setSupportActionBar(toolbar)
-        supportActionBar?.setHomeButtonEnabled(true)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-
         RichtarJakupApp.beersComponent.inject(this)
 
-        beer = intent.extras.getSerializable(BEER_ARG) as Beer
+        initToolbar()
+        startLoadingAnimation()
 
+        beer = intent.extras.getSerializable(BEER_ARG) as Beer
         title = beer.name
 
         beerManager.getBeerDetail(beer.detail)
@@ -46,13 +46,18 @@ class BeerDetailActivity: AppCompatActivity() {
                 .subscribe({
                     Picasso.with(this)
                             .load(it.image)
-                            .into(beer_image)
+                            .into(beer_image, object: Callback {
+                                override fun onSuccess() {
+                                    beer_description.text = Html.fromHtml(it.text)
+                                    view_switcher.displayedChild = 1
+                                    loading_image.clearAnimation()                                }
 
-                    beer_description.text = Html.fromHtml(it.text)
+                                override fun onError() {}
+                            })
                 },
-                        {
+                {
                             Toast.makeText(ScrollingActivity@this, it.message, Toast.LENGTH_SHORT).show()
-                        })
+                })
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -64,5 +69,16 @@ class BeerDetailActivity: AppCompatActivity() {
         }
 
         return super.onOptionsItemSelected(item)
+    }
+
+    private fun initToolbar() {
+        setSupportActionBar(toolbar)
+        supportActionBar?.setHomeButtonEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    private fun startLoadingAnimation() {
+        val myFadeInAnimation = AnimationUtils.loadAnimation(this, R.anim.loading)
+        loading_image.startAnimation(myFadeInAnimation)
     }
 }
